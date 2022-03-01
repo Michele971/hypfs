0fbf7de3216e93b912b4c5ca45340b7bfebe8b09
miker83z <mikerz83@gmail.com>
2022-03-01 11:38:17 +0100

    install with docker

---
+++ client/src/config.py
@@ -0,0 +1,10 @@
+import os
+
+APP_NAME = os.environ['APP_NAME']
+
+LOCAL_HOST = os.environ['LOCAL_HOST']
+INIT_PORT = int(os.environ['INIT_PORT'])
+HOP_SERVER_PORT = int(os.environ['HOP_SERVER_PORT'])
+
+HYPERCUBE_SIZE = int(os.environ['HYPERCUBE_SIZE'])
+SUPERSET_THRESHOLD = int(os.environ['SUPERSET_THRESHOLD'])
