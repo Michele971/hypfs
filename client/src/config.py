b503719f6d29b60a2ebb5e63920056ea386c983b
“Michele” <bonini.michele@yahoo.it>
2022-06-22 17:36:15 +0200

    changes to execute in local mode

--- client/src/config.py
+++ client/src/config.py
@@ -1,10 +1,23 @@
 import os
 
-APP_NAME = os.environ['APP_NAME']
+# APP_NAME = os.environ['APP_NAME']
 
-LOCAL_HOST = os.environ['LOCAL_HOST']
-INIT_PORT = int(os.environ['INIT_PORT'])
-HOP_SERVER_PORT = int(os.environ['HOP_SERVER_PORT'])
+# LOCAL_HOST = os.environ['LOCAL_HOST']
+# INIT_PORT = int(os.environ['INIT_PORT'])
+# HOP_SERVER_PORT = int(os.environ['HOP_SERVER_PORT'])
 
-HYPERCUBE_SIZE = int(os.environ['HYPERCUBE_SIZE'])
-SUPERSET_THRESHOLD = int(os.environ['SUPERSET_THRESHOLD'])
+# HYPERCUBE_SIZE = int(os.environ['HYPERCUBE_SIZE'])
+# SUPERSET_THRESHOLD = int(os.environ['SUPERSET_THRESHOLD'])
+
+
+
+APP_NAME = 'hypercube-client'
+
+LOCAL_HOST = '127.0.0.1'
+INIT_PORT = 8880
+HOP_SERVER_PORT = 6000
+
+
+HYPERCUBE_SIZE = 6
+
+SUPERSET_THRESHOLD = 10
