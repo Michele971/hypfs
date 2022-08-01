fdd406dda5a3f42da104d2e1c03a26927a63c63a
“Michele” <bonini.michele@yahoo.it>
2022-08-01 19:07:36 +0200

    interactions prover-witness

--- SmartContract_UI/test-suite/simulation.py
+++ SmartContract_UI/test-suite/simulation.py
@@ -1,7 +1,38 @@
 from openlocationcode import openlocationcode as olc
+import random
+import math
 
-WITNESS_NUMBER = 1
-PROVER_NUMBER = 1
+'''
+    NOTEs:
+    - Since we don't simulate the bluetooth feature during the process that finds neighbors,
+      we assume that if the Open Location Code, between two or more users, is the same, then the 
+      distance is very small. To assume that, we'll have to use the default OLC precision of 10 digits
+      which consist in a range of 14m.
+      We are aware that users in two different squares can be close althought the OLC is different, however we
+      don't simulate that.
+'''
+
+WITNESS_NUMBER = 4
+PROVER_NUMBER = 2
+DID_LIST_WIT = [0, 3, 4, 5]
+DID_LIST_PROV = [2, 6]
+
+LOCATION_LIST_WIT = ["7H369FXP+FH", "7H369F4W+Q8"]
+LOCATION_LIST_PROV = ["7H369FXP+FH", "7H369F4W+Q8"]
+
+dictOfLocation = {
+    "7H369FXP+FH":[
+        0,
+        3,
+        4,
+        5
+    ],
+    "7H369F4W+Q8":[
+        2,
+        6
+    ]
+}
+
 
 class Witness:
     def __init__(self, did, public_key, private_key, proofs_array_computed, location):
@@ -11,7 +42,10 @@ class Witness:
         self.proofs_array_computed = proofs_array_computed
         self.location = location
     
-    def verify_location_Prover(self, locationProver):
+    '''
+        This method will compute the proof when require from provers
+    '''
+    def comput_location_Prover(self, locationProver):
         # send proof
         pass
     
@@ -20,8 +54,12 @@ class Witness:
 
         In the real case we'll use the bluetooth range, so this function might not exists.
     '''
-    def computed_distance_from_prover(self, locationWitnes, locationProver):
-        pass
+    def computed_distance_from_prover(self, olc_witness, olc_prover):
+        if (olc_witness == olc_prover):
+            return True
+        else:
+            return False
+        
 
 
 
@@ -30,6 +68,18 @@ class Prover(Witness):
     def __init__(self, did, public_key, private_key, proofs_array_computed, location, proofs_received_array):
         super().__init__(did, public_key, private_key, proofs_array_computed, location)
         self.proofs_received_array = proofs_received_array
+
+    '''
+        This method will return the list of neihbours. 
+        listWitnessLocation is the input dict that own the location of every users:
+            - the Key is the position OLC
+            - the Value is an List of DID
+    '''
+    def find_neighbours(self, locationProver, dicWitnessLocation):
+        if (dicWitnessLocation.get(locationProver)):
+            return dicWitnessLocation.get(locationProver)
+        else: 
+            print('No Neighbours found in your location: ', locationProver)
 
     def send_location(self):
         # return proof
@@ -40,8 +90,8 @@ class Prover(Witness):
 
         In the real case we'll use the bluetooth range, so this function might not exists.
     '''
-    def computed_distance_from_witness(self, locationWitnes, locationProver):
-        super(Prover, self).computed_distance_from_prover(locationWitnes, locationProver)
+    def computed_distance_from_witness(self, olc_witness, olc_prover):
+        super(Prover, self).computed_distance_from_prover(olc_witness, olc_prover)
         pass
 
     ''' 
@@ -78,6 +128,10 @@ def createProver(did, public_key, private_key, proofs_array_computed, location, 
     
     return prov
 
+''' 
+    We'll use  Open Location Code format.
+    This is ideally suited for people that live in rural areas and don’t have access to an address.
+'''
 def generateOLC(latitude, longitude):
     location_encoded = olc.encode(latitude, longitude) #lat - long - N° digits. Default is 10 digits which allow 14m of precisions
     print('Encoded location: ', location_encoded)
@@ -85,14 +139,17 @@ def generateOLC(latitude, longitude):
 
 def startSimulation():
     #generateOLC
-
-    # for i in WITNESS_NUMBER
-    wit = createWitness(  
-        did= "0",
-        public_key= "SHJDAJAKRHAKD",
-        private_key= "xxxxx",
-        proofs_array_computed= [],
-        location= "7H368FJQ+84")
+    generateOLC(11.356988, 44.495858)
+    print(random.uniform(0, 1))
+    for i in range(0,WITNESS_NUMBER):
+        wit = createWitness(  
+            did= DID_LIST_WIT[i],
+            public_key= "SHJDAJAKRHAKD",
+            private_key= "xxxxx",
+            proofs_array_computed= [],
+            location= LOCATION_LIST_WIT[math.floor(random.uniform(0, 1))]) #random number between 0 and 1
+        #insert the wit in the dict of neighbour
+        dictOfLocation['wit.did']=wit.location
 
 
     # for i in PROVER_NUMBER
@@ -109,10 +166,14 @@ def startSimulation():
 
     # Move the Prover
 
-    print(olc.isValid(prov.location))
+    # print(olc.isValid(prov.location))
+    # print(wit.computed_distance_from_prover(wit.location, prov.location))
 
 
-
+    # Find neighbours
+    locationProver = prov.location
+    neighbours = prov.find_neighbours(locationProver, dictOfLocation)
+    print('Your neighbours are: ', neighbours)
 
 
 
