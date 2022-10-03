60d9a30e4776946f2d689ffefa46cead6a7cd457
“Michele” <bonini.michele@yahoo.it>
2022-10-03 18:27:40 +0200

    BIG refactoring

--- SmartContract_UI/test-suite/startSimulation.py
+++ SmartContract_UI/test-suite/startSimulation.py
@@ -1,4 +1,3 @@
-
 from platform import python_branch
 from openlocationcode import openlocationcode as olc
 from reach_rpc import mk_rpc
@@ -11,26 +10,93 @@ import numpy as np
 import matplotlib.pyplot as plt
 from makeTransaction import *
 import concurrent.futures
+import random
 '''
     ---------------------------------------------------------------------------------------
     ------------------    THIS SCRIPT MUST BE RUN ON ALGORAND TESTNET    ------------------
     ---------------------------------------------------------------------------------------
 '''
+SMART_CONTRACT_MAX_USER = 3 # this is the same variable of index.rsh. They must be equals!
+
+LOCATION_LIST_PROV = ["7H369F4W+Q8", "7H369F4W+Q9", "7H368FRV+FM", "7H368FWV+X6"] # list of Provers locatios. Used for build the prover object
+PROVER_NUMBER = 16 # number of provers for the entire system
+
+assert(PROVER_NUMBER/len(LOCATION_LIST_PROV) == 4) #There must be (SMART_CONTRACT_MAX_USER+1) users for each location. So increase the number of locations in LOCATION_LIST_PROV, or decrease the PROVER_NUMBER
+
+DID_list = [] #will be fill at runtime
+mapping_list_did = {} #will be fill at runtime
+
+'''
+    This method will generate n° prover objects setting their attribute such as DID, location or the consensus network account.
+    The DID created will be unique.
+    This method will create mapping_list_did dictionary, which will be used to check the users nearby.
+'''
+def generateProvers(n_users_to_generate):
+    proversObjectList = []
+    count = 0
+    for i in range(n_users_to_generate):
+        #use to assign each ID to the location
+        if count == len(LOCATION_LIST_PROV):
+            count = 0
+
+        # choose the location from the list of locations
+        chooseLocation = LOCATION_LIST_PROV[count] #there are SMART_CONTRACT_MAX_USER + 1 elements for each location
+        goOn = True
+        #generate a random ID that does not still exists
+        while goOn == True:
+            DID_generated = random.randint(0, 100)
+            if DID_generated not in DID_list:
+                DID_list.append(DID_generated)
+                goOn = False
+            else:
+                goOn = True
+
+        # update the map that keep track of the locations and their users 
+        mapping_list_did.setdefault(chooseLocation,[]).append(DID_generated) #the dict will be used to find the neighbours and othe operations
+
+        #create a Prover object
+        prov = createProver(
+            did= DID_generated, 
+            account= "Noone",
+            private_key= "None",
+            proofs_array_computed= [],
+            location= chooseLocation, 
+            proofs_received_array=[],
+            title_report="Report Title here ...",
+            description_report="Descrition of the report ...",
+            tx_id_data="None")
+        
+        #update some attributes of the prover objects
+        account_prov = prov.createAccount(i) #passing the number of prover to create
+        prov.account = account_prov
+        rpc("/acc/setGasLimit", account_prov, 5000000)
+
+        prover_addresses.append(format_address(account_prov))
+
+        proversObjectList.append(prov)
+
+        count += 1
+
+    return proversObjectList
+
+
 
 list_private_public_key = [
     'enact spoon inquiry wolf wait process weather earn raven glare winner enter tell mandate cement harbor garment problem crowd banner replace lounge sight abstract topple', # GIM3KUP5473BIGGJZ3GNDREHC2YT2P4W4WJZXDILWLAYI57A4GAML33DRY
     'honey city during awkward flush destroy decrease hero rhythm fog insect year miss ship mention wage fan crop angle harsh demise banner addict absent what', #O47RCDX6L725MCPKEQNWWCMZAMR3UNIUYSKKV7N5UEAZDC2A6HPPID4C7Q
     'seed record photo dress unique that uniform urban bone ritual drive nose wolf miracle neither old gentle trend stage crisp rate fitness legal ability empty', #C5PCYMHJJPL5HBVL7KOJ5EXXNYOZCWYRKEP5NMNC6Z44CQLWABDDSZFBKI
     'describe reunion balcony empower pool work gown armed supreme negative garage minute crucial endless robust divide accuse resemble lava announce share quarter dentist abstract sight', #7EH36BDEHMHFYJUISRA4QZYIHCCN5U4ZEQ3Q64T7OAYSMIRARZ3Q6C6F3E
+    
     'stool west normal magic option valve enrich vibrant knee total always shrug volcano fury ignore law second portion few uncover disease wreck green absorb bag', #5RW4HL5YY6FMYSICN5LYCCWM4LRXGNGJPU3RYW3SVRKADB7N2Z7OOIKZ7E
     'nose shell amount fossil manual letter shove cage damage fault initial insect border assume draft dawn number market giant movie parrot gentle police able hospital', #X56EISCBSSPAS75S2RZFJVBEXMGCVI2WG4QMIHRRCAHUGTCUGF7RE6643M
     'stable actress ordinary coral end potato approve coyote swap armed color clock eyebrow reject section thank host sense agree put sure replace area absorb echo', #2PDAQC47IFBJNU5AETJA2OV3I6NKDF4AAIZRG72ALDXMAUCEOO4KZPOO4E
     'people menu yellow vote twist guitar bargain will horse mammal usual hammer enroll input liar flower noise shy window ill clinic grape bar abstract estate', #BDFYNQR2I3YQR6WMTBYRYDJXCXDLIW5T22DVKT4IVL6R4ZQVI336E3VDNQ
+    
     'lift decade sausage turtle pipe cup piece caution educate carpet provide barrel asset library topic hood flip swallow hotel assist dignity winner chimney absorb cost', #3YMI5U6ZKH3MZ3DNYWO3YL3SCH75KBBCWO2XOO2HEVFQSX3G2Y4UYPOWWA
     'divorce ecology panel wash curious rich chunk spy piece position hip great random fashion rice visual obey powder borrow chief fade sibling art able borrow', #7TIHVKNIGJF5H37SJDWWKOOT4FRUE4XDUGCZ77HZVKAEAHAWKLSS2LXRUQ
-    
     'steak gauge carpet assume swamp tuna orphan athlete script inhale garment quiz critic exotic among turn obscure kitten modify width jacket tilt miss ability fence', # HKCHIEF5XKK6NYAYUBWHTX3JB7H62QQ36RWPUPNBGL75SXQB27ZWX5Q6DQ
     'chaos average someone regular luggage arena era parrot reason imitate yellow buyer clean enrich library pulse buddy shaft flip finger flip network theme abandon proof', # 22X4O3I5FSGYK4XTMWL6OEPKX7FPBI3QH4BB42VIZ75J7GVUDJZYCJ56OA
+    
     'escape leg warm silver biology silk swallow wet ticket decrease seat speed drift nice give clerk find gather verb cube assume around gather abandon month', # QF25BWUJIY3NDFS47EILAOV4DAAHIYC2PRVIGERSQVTJ7RXHY44QBA6KO4
     'decline auction milk please lens quiz eight rough affair possible forum brand sing bless comfort believe exist twenty invest apart mask gesture match abstract marine', # MJXSIP7ULI25QH3XJP7AVQT7ZYO3TARYVUYRD6ARR6L6HWGH22ECU5MHAE
     'whisper giggle spot sadness loyal menu cliff sense flash share guide exit oak loud field inch during slender clump loyal stove enter album abstract wool', # XSKXAH4SIWFOLFUWS6H73JUJ5HJOWUCBKTA6LFXFCYS6VDZMQL6RI5HGQM
@@ -48,62 +114,6 @@ rpc("/stdlib/setProviderByName","TestNet")
 
 print("\t\t The consesus network is: ", rpc('/stdlib/connector'))
 STARTING_BALANCE = rpc("/stdlib/parseCurrency", 1500) 
-location_in_hypercube = False # simulate if the location is already stored in hypercube
-
-
-PROVER_NUMBER = 4 # number of provers for the entire system
-
-'''
-    ❗️❗️❗️❗️  WARNING: ❗️❗️❗️❗️
-    ---> len(DID_LIST_PROV) and len(LOCATION_LIST_PROV) MUST TO BE EQUALS !!! You can decrease the PROVER_NUMBER during the testing
-    ---> PROVER_NUMBER are all the provers of the system
-'''
-DID_LIST_PROV = [2, 6, 50, 51, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22] # DID of provers that will ask for a Proof Of Location and a verify process
-LOCATION_LIST_PROV = ["7H369F4W+Q8", "7H369F4W+Q8", "7H369F4W+Q8", "7H369F4W+Q8", "7H369F4W+Q9", "7H369F4W+Q9", "7H369F4W+Q9", "7H369F4W+Q9", "7H368FRV+FM", "7H368FRV+FM", "7H368FRV+FM", "7H368FRV+FM", "7H368FWV+X6", "7H368FWV+X6", "7H368FWV+X6", "7H368FWV+X6", "7H369FXP+FH"] # list of Provers locatios. Used for build the prover object
-
-
-#### We know the position of every witness because it is stored in dictOfLocation. The position is the KEY, the values are the DID of user in that position
-dictOfLocation = {
-    "7H369FXP+FH":[
-        0, 
-        3,
-        4,
-        5
-    ],
-    "7H369F4W+Q8":[
-        2,
-        6,
-        50,
-        51
-    ],
-    "7H369F4W+Q9":[
-        8,
-        9,
-        10,
-        11
-    ],
-    "7H368FRV+FM":[ #Bologna
-        12,
-        13,
-        14,
-        15
-    ],
-    "7H368FWV+X6": [ #Ice-cream Bologna
-        16,
-        17,
-        18,
-        19
-    ], 
-    "7H369FXP+FH": [ # Ranzani 13: 44.4864416,11.3986586
-        22,
-        30,
-        44,
-        26
-    ]
-            
-}
-
-NUMBER_OF_LOCATIONS = 6 #number of different locations. For each location there could be a smart contract
 
 
 class Witness:
@@ -179,33 +189,11 @@ class Prover(Witness):
         else: 
             print('No Neighbours found in your location: ', locationProver)
 
-    '''
-        This method allow to the prover to compute the distance from the witness.
 
-        In the real case we'll use the bluetooth range, so this function might not exists.
-    '''
-    def computed_distance_from_witness(self, olc_witness, olc_prover):
-        super(Prover, self).computed_distance_from_prover(olc_witness, olc_prover)
-        pass
-
-    ''' 
-        When a user want to send his location to the smart contract,
-        we have to check if the location is already sent (checking inside the
-        hypercube).
-        
-        If the location is still not sent, user will have to deploy the contract.
-    '''
-    def retrieve_position_hypercube(self):
-        # Check hypercube
-
-        # Deploy smart contract if location is not in the hypercube
-        pass
 
     def createAccount(self, i):
         # ########### #######  WORK WITH REACH DEVNET ##################
         #acc_prover = rpc("/stdlib/newTestAccount", STARTING_BALANCE)
-
-
         
         # use newAccountFromMnemonic for Algorand testnet
         acc_prover = rpc("/stdlib/newAccountFromMnemonic", list_private_public_key[i])
@@ -231,16 +219,6 @@ class Prover(Witness):
         return attacherThread
 
 
-
-def createWitness(did, public_key, private_key, proofs_array_computed, location):
-    wit = Witness(
-        did= did, # Decentralized IDentifier 
-        public_key= public_key, # Public key of the wallet
-        private_key= private_key, # Private key of the wallet
-        proofs_array_computed= proofs_array_computed, # Witness will store every proof that has computed for someone (in the local mobile db e.g Room Database with Android) 
-        location= location)
-
-    return wit
 
 def createProver(did, account, private_key, proofs_array_computed, location, proofs_received_array, title_report, description_report, tx_id_data):
     prov = Prover(
@@ -268,30 +246,18 @@ def generateOLC(latitude, longitude):
 
 # START the simulation
 def startSimulation():
+    #generate N random provers
+    generate_prover_list = generateProvers(PROVER_NUMBER) # try with 8, 12, 16 etc.
+
+    print("\n\n----------- START -----------")
     dict_location_sc = {} # keep track if the smart contract is newAccountFromMnemonicalready associated to this particular location. Its lenght will be equal to NUMBER_OF_LOCATIONS
-    
+
     # Starting prover steps
     for i in range(0, PROVER_NUMBER): #for every prover of the entire system ...
         ##### TODO: Generate random LATITUDE & LONGITUDE (for every user), Then convert them to Open Location code and add to LOCATION_LIST_PROV
         #generateOLC(11.3474453,44.4930181 )#11.356988, 44.495888) # just for testing
-        #buildDict()
 
-        prov = createProver(
-            did= DID_LIST_PROV[i], # The Prover ID come from an default array that contains all the IDs
-            account= "None",
-            private_key= "None",
-            proofs_array_computed= [],
-            location= LOCATION_LIST_PROV[i], # The Prover Location come from an default array that contains all the Locations
-            proofs_received_array=[],
-            title_report="Report Title here ...",
-            description_report="Descrition of the report ...",
-            tx_id_data="None")
-        
-        account_prov = prov.createAccount(i) #passing the number of prover to create
-        # TODO: create a list of object provers and remove the two line below. Refactoring
-        prover_list_account.append(account_prov)
-        prover_addresses.append(format_address(account_prov)) #getting the wallet addresses for prover and appending to the list
-        prov.account = account_prov
+        prov = generate_prover_list[i]
 
         #store data inside the blockchain
         # with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -301,7 +267,7 @@ def startSimulation():
         #     prov.tx_id_data = tx_id
 
         # Find neighbours
-        neighbours = prov.find_neighbours(prov.location, dictOfLocation)
+        neighbours = prov.find_neighbours(prov.location, mapping_list_did)
         if neighbours: 
 
             print('→ 🪪 Prover DID: ', prov.did,'\n 📍 Location: ', prov.location, '\n    Neighbours: ', neighbours,'\n',)
@@ -344,14 +310,17 @@ def startSimulation():
         "O47RCDX6L725MCPKEQNWWCMZAMR3UNIUYSKKV7N5UEAZDC2A6HPPID4C7Q",
         "C5PCYMHJJPL5HBVL7KOJ5EXXNYOZCWYRKEP5NMNC6Z44CQLWABDDSZFBKI",
         "7EH36BDEHMHFYJUISRA4QZYIHCCN5U4ZEQ3Q64T7OAYSMIRARZ3Q6C6F3E",
+        
         "5RW4HL5YY6FMYSICN5LYCCWM4LRXGNGJPU3RYW3SVRKADB7N2Z7OOIKZ7E",
         "X56EISCBSSPAS75S2RZFJVBEXMGCVI2WG4QMIHRRCAHUGTCUGF7RE6643M",
         "2PDAQC47IFBJNU5AETJA2OV3I6NKDF4AAIZRG72ALDXMAUCEOO4KZPOO4E",
         "BDFYNQR2I3YQR6WMTBYRYDJXCXDLIW5T22DVKT4IVL6R4ZQVI336E3VDNQ",
+        
         "3YMI5U6ZKH3MZ3DNYWO3YL3SCH75KBBCWO2XOO2HEVFQSX3G2Y4UYPOWWA",
         "7TIHVKNIGJF5H37SJDWWKOOT4FRUE4XDUGCZ77HZVKAEAHAWKLSS2LXRUQ",
         "HKCHIEF5XKK6NYAYUBWHTX3JB7H62QQ36RWPUPNBGL75SXQB27ZWX5Q6DQ",
         "22X4O3I5FSGYK4XTMWL6OEPKX7FPBI3QH4BB42VIZ75J7GVUDJZYCJ56OA",
+        
         "QF25BWUJIY3NDFS47EILAOV4DAAHIYC2PRVIGERSQVTJ7RXHY44QBA6KO4",
         "MJXSIP7ULI25QH3XJP7AVQT7ZYO3TARYVUYRD6ARR6L6HWGH22ECU5MHAE",
         "XSKXAH4SIWFOLFUWS6H73JUJ5HJOWUCBKTA6LFXFCYS6VDZMQL6RI5HGQM",
@@ -384,7 +353,10 @@ def startSimulation():
     plt.xticks(x_pos, bars)
 
     # Show graphic
-    plt.show()
+    #plt.show()
+
+    plt.savefig('./outputPerformance.png')
+    
 
             
 def main():
